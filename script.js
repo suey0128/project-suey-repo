81840fd0721b510493a12bef50c5d768d68e53e7
suey0128 <syui0128@gmail.com>
2021-05-07 10:11:37 -0700

    website ver 1.0

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+/*
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
 
 document.querySelector('body').appendChild(h2); 
+
+access css window.getComputedStyle()
+
+
+
+const qualityCardsP = document.querySelector(".qualityCardsP");
+
+
+function ShowAndHide() {
+    let qualityCardsPDisplay = window.getComputedStyle(qualityCardsP).display;
+    if (qualityCardsPDisplay === "none") {
+        qualityCardsPDisplay = "block";
+    } else {
+        qualityCardsPDisplay = "none";
+    }
+}
+
+function ShowAndHide() {
+    var x = document.getElementById('SectionName');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+const qualityCards = document.querySelector(".qualityCards")
+
+qualityCards.addEventListener("hover", ShowAndHide) 
+
+
+
+*/
